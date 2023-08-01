4e6b9d85db33a26f6a3829d209a87c82a9c97ff1
Veer966 <veersinghnyc@gmail.com>
2023-07-31 22:37:49 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Add your code to this file
+document.body.style.backgroundColor = "rgb(0, 0, 100)";
+
+const pokemonImages = document.querySelectorAll(".pokemon_images");
+
+pokemonImages.forEach(image => {
+  image.style.border = "2px dotted orange";
+});
+const h3Elements = document.querySelectorAll("h3");
+
+h3Elements.forEach(h3 => {
+  h3.style.color = "rgb(255, 255, 160)";
+});
